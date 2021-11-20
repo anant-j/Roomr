841716026535e762297d76dc86532ab7826a10b0
Anant Jain <anant.j2409@gmail.com>
2021-11-19 19:29:18 -0500

    Added basic task list

---
+++ app/components/Task.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+
+const Task = (props:any) => {
+
+  return (
+    <View style={styles.item}>
+      <View style={styles.itemLeft}>
+        <View style={styles.circle}></View>
+        <Text style={styles.itemText}>{props.text}</Text>
+      </View>
+      <View 
+    //   style={styles.itemDate}
+      >
+          {/* {props.date} */}
+          <Text style={styles.itemDate}>Nov 15</Text>
+          </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  item: {
+    backgroundColor: '#FFF',
+    padding: 15,
+    borderRadius: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    // marginBottom: 20,
+  },
+  itemLeft: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    flexWrap: 'wrap'
+  },
+  circle: {
+    width: 24,
+    height: 24,
+    borderColor: '#55BCF6',
+    borderWidth: 2,
+    opacity: 0.8,
+    borderRadius: 50,
+    marginRight: 15,
+  },
+  itemText: {
+    fontSize: 17,
+    fontWeight: "500",
+    maxWidth: '80%',
+  },
+  itemDate: {
+    fontSize: 15,
+    fontWeight: "400",
+   },
+});
+
+export default Task;
--- app/screens/Tenant/HomeScreen.tsx
+++ app/screens/Tenant/HomeScreen.tsx
@@ -1,14 +1,81 @@
 import * as React from "react";
-import { StyleSheet } from "react-native";
+// import { StyleSheet } from "react-native";
 import { Text, View } from "../../components/Themed";
 import { RootTabScreenProps } from "../../types";
+import { KeyboardAvoidingView, StyleSheet, TextInput, TouchableOpacity, Keyboard, ScrollView, Platform } from 'react-native';
+import Task from '../../components/Task';
+import { useState } from "react";
 
 export default function HomeScreen({
   navigation,
 }: RootTabScreenProps<"TabOne">) {
+  const [task, setTask] = useState<string | undefined>();
+  const [taskItems, setTaskItems] = useState<any[]>([]);
+  const handleAddTask = () => {
+    Keyboard.dismiss();
+    setTaskItems([...taskItems, task])
+    setTask("");
+  }
+
+  const completeTask = (index:number) => {
+    let itemsCopy = [...taskItems];
+    itemsCopy.splice(index, 1);
+    setTaskItems(itemsCopy)
+  }
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Home</Text>
+        {/* Added this scroll view to enable scrolling when list gets longer than the page */}
+        <ScrollView
+        contentContainerStyle={{
+          flexGrow: 1
+        }}
+        keyboardShouldPersistTaps='handled'
+      >
+
+      {/* Today's Tasks */}
+      <Text style={styles.sectionSubTitle}>Landlord</Text>
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Upcoming Tasks</Text>
+        <View style={styles.items}>
+          {/* This is where the tasks will go! */}
+          {
+            taskItems.map((item, index) => {
+              return (
+                <TouchableOpacity key={index} 
+                 onPress={() => completeTask(index)}
+                 >
+                  <Task text={item} /> 
+                  <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
+                </TouchableOpacity>
+              )
+            })
+          }
+        </View>
+      </View>
+        
+      </ScrollView>
+
+      {/* Write a task */}
+      {/* Uses a keyboard avoiding view which ensures the keyboard does not cover the items on screen */}
+      <KeyboardAvoidingView 
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.writeTaskWrapper}
+      >
+        <TextInput style={styles.input} placeholder={'Write a task'} value={task} 
+        onChangeText={text => setTask(text)}
+         />
+        <TouchableOpacity
+         onPress={() => handleAddTask()}
+         >
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   );
 }
@@ -16,16 +83,55 @@ export default function HomeScreen({
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    // backgroundColor: '#E8EAED',
   },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
+  tasksWrapper: {
+    paddingTop: 10,
+    paddingHorizontal: 30,
+  },
+  sectionSubTitle: {
+    fontSize: 17,
+    // fontWeight: 'bold',
+    color: '#8A8F9E',
+    paddingTop: 10,
+    paddingHorizontal: 30,
+  },
+  sectionTitle: {
+    fontSize: 30,
+    fontWeight: 'bold'
+  },
+  items: {
+    marginTop: 10,
+  },
+  writeTaskWrapper: {
+    position: 'absolute',
+    bottom: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+    width: 250,
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
   },
   separator: {
-    marginVertical: 30,
     height: 1,
-    width: "80%",
   },
+  addText: {},
 });
