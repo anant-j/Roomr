421603107c7ef1c153d6c2a27cc5290588a4f58d
Anant Jain <anant.j2409@gmail.com>
2021-11-20 00:55:20 -0500

    Update HomeScreen.tsx

--- app/screens/Tenant/HomeScreen.tsx
+++ app/screens/Tenant/HomeScreen.tsx
@@ -28,42 +28,42 @@ export default function HomeScreen({
   return (
     <View style={styles.container}>
       {/* Added this scroll view to enable scrolling when list gets longer than the page */}
- 
+
       <View style={styles.topContainer}>
-      {/* Today's Tasks */}
-      <Text style={styles.sectionSubTitle}>Tenant</Text>
-      <View style={styles.tasksWrapper}>
-        <View style={styles.titleWrapper}>
-          <Text style={styles.sectionTitle}>Upcoming Tasks</Text>
-          {/* <Text style={styles.sectionAddButton}>+</Text> */}
-          <Feather name="plus-circle" size={24} onPress={handleAddTask} color="black" />
-        </View>
-        <ScrollView
-        contentContainerStyle={{
-          flexGrow: 1
-        }}
-        keyboardShouldPersistTaps='handled'
-      >
-        <View style={styles.items}>
-          {/* This is where the tasks will go! */}
-          {
-            taskItems.map((item, index) => {
-              return (
-                <TouchableOpacity key={index}
-                  onPress={() => completeTask(index)}
-                >
-                  <Task text={item} />
-                  <View
-                    style={styles.separator}
-                    lightColor="#eee"
-                    darkColor="rgba(255,255,255,0.1)"
-                  />
-                </TouchableOpacity>
-              )
-            })
-          }
-        </View>
-        </ScrollView>
+        {/* Today's Tasks */}
+        <Text style={styles.sectionSubTitle}>Tenant</Text>
+        <View style={styles.tasksWrapper}>
+          <View style={styles.titleWrapper}>
+            <Text style={styles.sectionTitle}>Upcoming Tasks</Text>
+            {/* <Text style={styles.sectionAddButton}>+</Text> */}
+            <Feather name="plus-circle" size={24} onPress={handleAddTask} color="black" />
+          </View>
+          <ScrollView
+            contentContainerStyle={{
+              flexGrow: 1
+            }}
+            keyboardShouldPersistTaps='handled'
+          >
+            <View style={styles.items}>
+              {/* This is where the tasks will go! */}
+              {
+                taskItems.map((item, index) => {
+                  return (
+                    <TouchableOpacity key={index}
+                      onPress={() => completeTask(index)}
+                    >
+                      <Task text={item} />
+                      <View
+                        style={styles.separator}
+                        lightColor="#eee"
+                        darkColor="rgba(255,255,255,0.1)"
+                      />
+                    </TouchableOpacity>
+                  )
+                })
+              }
+            </View>
+          </ScrollView>
         </View>
       </View>
 
