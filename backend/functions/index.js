0f74070be98bfee7dcdafd0fc59b0947f79a8a9e
Anant Jain <anant.j2409@gmail.com>
2022-01-23 20:01:56 -0500

    Added backend method to send notification

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,9 +1,59 @@
+const {default: Expo} = require("expo-server-sdk");
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
+const db = admin.firestore();
+const expo = new Expo();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+exports.sendNotification = functions.https.onRequest(async (req, res) => {
+  const notificationMessage = req.query.message;
+  if (!notificationMessage) {
+    return res.status(400).send("No message specified");
+  }
+  const notificationRef = await
+  db.collection("users").doc("notifications").get();
+  if (notificationRef.exists) {
+    const notificationList = notificationRef.data();
+    sendExpoNotifications(notificationMessage, notificationList.ids);
+    res.send("Messages sent");
+    return;
+  }
+  res.send("Done");
 });
+
+/**
+ * @param  {string} message
+ * @param  {Array} tokens
+ */
+async function sendExpoNotifications(message, tokens) {
+  const messages = [];
+  for (const pushToken of tokens) {
+    if (!Expo.isExpoPushToken(pushToken)) {
+      console.error(`Push token ${pushToken} is not a valid Expo push token`);
+      continue;
+    }
+
+    messages.push({
+      to: pushToken,
+      sound: "default",
+      body: message,
+    });
+  }
+
+  const chunks = expo.chunkPushNotifications(messages);
+  const tickets = [];
+  (async () => {
+    for (const chunk of chunks) {
+      try {
+        const ticketChunk = await expo.sendPushNotificationsAsync(chunk);
+        tickets.push(...ticketChunk);
+        // NOTE: If a ticket contains an error code in ticket.details.error, you
+        // must handle it appropriately. The error codes are listed in the Expo
+        // documentation:
+        // https://docs.expo.io/push-notifications/sending-notifications/#individual-errors
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  })();
+}
