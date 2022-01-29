444dc4b4202ce5e058f88438fb9d37169b5a3f75
Taha Zeeshan Khan <khant21@mcmaster.ca>
2022-01-29 17:37:15 -0500

    save

--- app/screens/Tenant/CalendarScreen.tsx
+++ app/screens/Tenant/CalendarScreen.tsx
@@ -1,6 +1,30 @@
+import { useAppSelector } from "hooks/typedHooks";
 import * as React from "react";
+import { useState } from "react";
+import { Agenda, AgendaSchedule, DateData } from "react-native-calendars";
 import AgendaScreen from "./AgendaScreen";
+// import AgendaScreen from "./AgendaScreen";
+
+interface AgendaState {
+  items?: AgendaSchedule;
+}
 
 export default function CalendarScreen() {
-  return <AgendaScreen />;
+  // return <AgendaScreen />;
+
+  const { allTasks } = useAppSelector((state) => state.tasks);
+
+  const initAgenda: AgendaState = { items: undefined };
+  const [calendarItems, setCalendarItems] = useState({ items: undefined });
+
+  return (
+    <Agenda
+      items={calendarItems}
+      loadItemsForMonth={(day) => loadTasksThisMonth(day, allTasks)}
+    />
+  );
 }
+
+const loadTasksThisMonth = (day: DateData, allTasks) => {
+  console.log(allTasks);
+};
