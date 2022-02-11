86b69b056f33037a83b8bf11990733993bae0d04
Anant Jain <anant.j2409@gmail.com>
2022-02-11 12:14:14 -0500

    No notifications on chat screen, but enabled otherwise

--- app/screens/Tenant/ChatScreen.tsx
+++ app/screens/Tenant/ChatScreen.tsx
@@ -7,6 +7,7 @@ import {
   StyleSheet,
   TouchableOpacity,
 } from "react-native";
+import * as Notifications from "expo-notifications";
 import { Button, Text, View, TextInput } from "components/Themed";
 import { useAppDispatch, useAppSelector } from "hooks/typedHooks";
 import { setActiveChat } from "reduxStates/chatSlice";
@@ -28,10 +29,19 @@ export default function ChatScreen() {
 
   const onChatPress = (id: string) => {
     onChangeMessage("");
+    Notifications.setNotificationHandler(null);
     dispatch(setActiveChat(id));
   };
 
   const onBackToChat = () => {
+    Notifications.setNotificationHandler({
+      handleNotification: async () => ({
+        shouldShowAlert: true,
+        shouldPlaySound: true,
+        shouldSetBadge: false,
+      }),
+    });
+
     dispatch(setActiveChat(""));
   };
 
